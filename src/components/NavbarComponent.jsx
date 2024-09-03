44848a3de1a6ff170ea6599bf874cb13785b2893
Fattan2005 <fattanhibrizi@gmail.com>
2024-09-03 14:55:53 +0700

    navbar part 2

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -3,29 +3,31 @@ import {navLinks} from '../data/index';
 import { NavLink } from 'react-router-dom';
 
 const NavbarComponent = () => {
-  return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="/">Ngoding</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto">
-            {navLinks.map((link) => {
-                return (
-                    <div className='nav-link' key={link.id}>
-                        <NavLink to={link.path}>{link.text}</NavLink>
-                    </div>
-                );
-            })}
-          </Nav>
+return (
+<Navbar expand="lg">
+  <Container>
+    <Navbar.Brand href="/" className='fs-3 fw-bold'>Ngoding.</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mx-auto text-center">
+        {navLinks.map((link) => {
+        return (
+        <div className='nav-link' key={link.id}>
+          <NavLink to={link.path} className={({ isActive, isPending })=> isPending ? "pending" : isActive ? "active" : ""} end>
+              {link.text}
+          </NavLink>
+        </div>
+        );
+        })}
+      </Nav>
 
-          <div>
-            <button> Join With Us</button>
-          </div>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  )
+      <div className='text-center'>
+        <button className='btn btn-outline-danger rounded01'> Join With Us</button>
+      </div>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+)
 }
 
 export default NavbarComponent
