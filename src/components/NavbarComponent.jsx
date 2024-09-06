452277ac30e7bf80141a6605d2843754b768ba4c
Fattan2005 <fattanhibrizi@gmail.com>
2024-09-06 18:53:54 +0700

    revisi navbar effect

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,48 +1,49 @@
 import { useState, useEffect } from 'react'; 
-import {Navbar, Container, Nav} from 'react-bootstrap';
-import {navLinks} from '../data/index';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { navLinks } from '../data/index';
 import { NavLink } from 'react-router-dom';
 
 const NavbarComponent = () => {
-const [changeColor, setChageColor] = useState(false);
+  const [changeColor, setChangeColor] = useState(false);
 
-const changeBackgroundColor = () => {
-  if(window.scrollY > 10){
-    setChageColor(true);
-  }else{
-    setChageColor(false);
-  }
-}
+  const changeBackgroundColor = () => {
+    if(window.scrollY > 10){
+      setChangeColor(true);
+    }else{
+      setChangeColor(false);
+    }
+  };
 
-useEffect(() =>{
-  changeBackgroundColor();
+  useEffect(() => {
+    changeBackgroundColor();
 
-  window.addEventListener('scroll', changeBackgroundColor)
-})
-return (
-<Navbar expand="lg" className={changeColor ? "color-active" : ""}>
-  <Container>
-    <Navbar.Brand href="/" className='fs-3 fw-bold'>Ngoding.</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mx-auto text-center">
-        {navLinks.map((link) => {
-        return (
-        <div className='nav-link' key={link.id}>
-          <NavLink to={link.path} className={({ isActive, isPending })=> isPending ? "pending" : isActive ? "active" : ""} end>
-              {link.text}
-          </NavLink>
+    window.addEventListener('scroll', changeBackgroundColor)
+  });
+
+  return (
+  <Navbar expand="lg" className={changeColor ? "color-active" : ""}>
+    <Container>
+      <Navbar.Brand href="/" className='fs-3 fw-bold'>Ngoding.</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mx-auto text-center">
+          {navLinks.map((link) => {
+          return (
+          <div className='nav-link' key={link.id}>
+            <NavLink to={link.path} className={({ isActive, isPending })=> isPending ? "pending" : isActive ? "active" : ""} end>
+                {link.text}
+            </NavLink>
+          </div>
+          );
+          })}
+        </Nav>
+
+        <div className='text-center'>
+          <button className='btn btn-outline-danger rounded01'> Join With Us</button>
         </div>
-        );
-        })}
-      </Nav>
-
-      <div className='text-center'>
-        <button className='btn btn-outline-danger rounded01'> Join With Us</button>
-      </div>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
 )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,13 +7,13 @@ const HomePage = () => {
         <header className="w-100 min-vh-100 d-flex align-items-center">
           <Container>
             <Row className="header-box d-flex align-items-center">
-              <Col>
-                <h1>Temukan <br /> <span>Bakat Kreatifmu</span> <br /> Bersama Kami</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, voluptates nemo voluptatum officia pariatur minima.</p>
+              <Col lg="6">
+                <h1 className="mb-4 ">Temukan <br /> <span>Bakat Kreatifmu</span> <br /> Bersama Kami</h1>
+                <p className="mb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, voluptates nemo voluptatum officia pariatur minima.</p>
                 <button className="btn btn-danger btn-lg rounded-1 me-2">Lihat Kelas</button>
                 <button className="btn btn-outline-danger btn-lg rounded-1">Lihat Promo</button>
               </Col>
-              <Col>
+              <Col lg="6">
                 <img src={HeroImage} alt="hero-img" />
               </Col>
             </Row>
