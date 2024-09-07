382e624c25771e3e0dd69cfb1ea22580be6b12ed
Fattan2005 <fattanhibrizi@gmail.com>
2024-09-07 18:25:44 +0700

    swipe testimonial

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,20 @@
 import { Container, Row, Col } from "react-bootstrap"
 import HeroImage from "../assets/img/hero.png"
 
-import { kelasTerbaru } from "../data/index";
+import { kelasTerbaru, dataSwiper } from "../data/index";
 import { useNavigate} from "react-router-dom";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
 
 const HomePage = () => {
   let navigate = useNavigate();
@@ -11,7 +23,7 @@ const HomePage = () => {
     <div className="homepage">
         <header className="w-100 min-vh-100 d-flex align-items-center">
           <Container>
-            <Row className="header-box d-flex align-items-center">
+            <Row className="header-box d-flex align-items-center pt-lg-5">
               <Col lg="6">
                 <h1 className="mb-4 ">Temukan <br /> <span>Bakat Kreatifmu</span> <br /> Bersama Kami</h1>
                 <p className="mb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, voluptates nemo voluptatum officia pariatur minima.</p>
@@ -34,7 +46,7 @@ const HomePage = () => {
             </Row>
             <Row>
               {kelasTerbaru.map((kelas) =>{
-                return <Col key={kelas.id}>
+                return <Col key={kelas.id} className="shadow rounded">
                     <img src={kelas.image} alt="unspalsh.com" className="rounded-top mb-5"/>
                     <div className="star mb-2 px-3">
                       <i className={kelas.star1}></i>
@@ -57,6 +69,59 @@ const HomePage = () => {
               <i className="fa-solid fa-chevron-right ms-1"></i>
               </button>
               </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="testimonial py-5">
+          <Container>
+            <Row>
+              <Col>
+                <h1 className="text-center fw-bold my-5">Testimonial</h1>
+              </Col>
+            </Row>
+            <Row>
+              <Swiper
+                slidesPerView={1}
+                spaceBetween={10}
+                pagination={{
+                  clickable: true,
+                }}
+                breakpoints={{
+                  640: {
+                    slidesPerView: 1,
+                    spaceBetween: 20,
+                  },
+                  768: {
+                    slidesPerView: 2,
+                    spaceBetween: 40,
+                  },
+                  992: {
+                    slidesPerView: 2,
+                    spaceBetween: 50,
+                  },
+                  1200: {
+                    slidesPerView: 3,
+                    spaceBetween: 50,
+                  },
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+              >
+                {dataSwiper.map((data) => {
+                  return (
+                    <SwiperSlide key={data.id} className="shadow-sm">
+                      <p className="desc">{data.desc}</p>
+                      <div className="people">
+                        <img src={data.image} alt="" />
+                        <div>
+                          <h5 className="mb-1">{data.name}</h5>
+                          <p className="m-0 fw-bold">{data.skill}</p>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                  );
+                })}
+              </Swiper>
             </Row>
           </Container>
         </div>
